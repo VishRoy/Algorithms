e5e5749197b72485f0c6c927c67d3e24982dda93
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-01-15 16:06:32 +0530

    Binary Search

--- Search.js
+++ Search.js
@@ -15,3 +15,26 @@ let arr = [1, 5,3, 4, 33, 9]
 findElement(arr, 33) 
 
 //Binary Search
+
+// Only works for sorted array
+
+function findElement(arr, element){
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+  while(startIndex <= endIndex){
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex)/2)
+    if(element == arr[middleIndex]){
+      return middleIndex
+    }
+    if(element < arr[middleIndex]){
+      endIndex = middleIndex - 1
+    }else{
+      startIndex = middleIndex + 1
+    }
+  }
+}
+
+
+let arr = [1, 3, 4, 5 , 33, 76, 89, 99]
+
+findElement(arr, 33)
