05f4d57121a1f1ec8d4b2579ee280c6eccfa4e98
Vishakha Roy <69618079+VishRoy@users.noreply.github.com>
2021-01-15 15:43:29 +0530

    Linear Search

--- Search.js
+++ Search.js
@@ -1,5 +1,17 @@
-//Linear Search
+//Linear Search - O(n)
+
+function findElement(arr, element){
+  for(let i=0;i<arr.length;i++){
+    if(arr[i] == element){
+      return i
+    }
+  }
+  return false
+}
 
 
+let arr = [1, 5,3, 4, 33, 9]
+
+findElement(arr, 33) 
 
 //Binary Search
